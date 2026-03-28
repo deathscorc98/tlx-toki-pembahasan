7713fa79c670490943d588b5827ed2d0df2682f0
deathscorc98 <nandaditra56@gmail.com>
2026-03-28 10:03:51 +0700

    solution for teman dekat, with time 4 s

---
+++ Pemrograman Dasar dengan C++/12. Latihan Pemantapan Materi/E. Teman Terdekat/solution.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, D;
+    cin >> N >> D;
+
+    vector<vector<int>> jarak(N, vector<int>(2));
+
+    for(int i=0;i<N;i++) {
+        for(int j=0;j<2;j++) {
+            cin >> jarak[i][j];
+        }
+    }
+
+    for(int i=0;i<N;i++) {
+        for(int j=0;j<2;j++) {
+            cout << jarak[i][j];
+        }
+        cout << endl;
+    }
+
+    vector<int> dekat;
+
+    for(int i=0;i<N-1;i++) {
+        for(int j=i+1;j<N;i++) {
+            int x = pow(abs(jarak[j][0]-jarak[i][0]), D);
+            int y = pow(abs(jarak[j][1]-jarak[i][1]), D);
+            int temp = x + y;
+            dekat.push_back(temp);
+        }
+    }
+
+    int min = dekat[0];
+    int max = 0;
+
+    for(int k=0;k<dekat.size();k++) {
+        if(dekat[k] <= min ) {
+            min = dekat[k];
+        }
+        
+        if(dekat[k] >= max) {
+            max = dekat[k];
+        }
+    }
+
+    cout << min << " " << max << endl;
+
+    return 0;
+}
